747c3ad0bf2e1c4fbd8a4f988adfc8aa48eec4c7
thomasjohnstapleton <thomasstapleton0@gmail.com>
2015-12-28 14:01:44 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Roman Numeral Converter [![Build Status](https://travis-ci.org/tomsta93/roman-numeral-converter.svg?branch=master)](https://travis-ci.org/tomsta93/roman-numeral-converter)
+Convert numbers to a Roman Numerals
+
+## Install
+
+`npm i -g roman-numeral-converter`
+
+## Use
+
+`roman-numeral-converter ${number}`
+
+Example: `roman-numeral-converter 10`
+
+## License
+
+MIT © [Thomas Stapleton](https://github.com/tomsta93)
---
+++ data/settings.js
@@ -0,0 +1,106 @@
+module.exports = [
+    {
+        rules: [
+            {
+                value: 4,
+                startSymbol: 'I',
+                setSymbol: 'V',
+                endSymbol: ''
+            },
+            {
+                value: 5,
+                startSymbol: '',
+                setSymbol: 'V',
+                endSymbol: ''
+            },
+            {
+                value: 9,
+                startSymbol: 'I',
+                setSymbol: 'X',
+                endSymbol: ''
+            }
+        ],
+        defaults: {
+            value: 'I',
+            lessThan: 4
+        }
+    },
+    {
+        rules: [
+            {
+                value: 4,
+                startSymbol: 'X',
+                setSymbol: 'L',
+                endSymbol: ''
+            },
+            {
+                value: 5,
+                startSymbol: '',
+                setSymbol: 'L',
+                endSymbol: ''
+            },
+            {
+                value: 9,
+                startSymbol: '',
+                setSymbol: 'X',
+                endSymbol: 'C'
+            }
+        ],
+        defaults: {
+            value: 'X',
+            lessThan: 4
+        }
+    },
+    {
+        rules: [
+            {
+                value: 4,
+                startSymbol: 'C',
+                setSymbol: 'D',
+                endSymbol: ''
+            },
+            {
+                value: 5,
+                startSymbol: '',
+                setSymbol: 'D',
+                endSymbol: ''
+            },
+            {
+                value: 9,
+                startSymbol: '',
+                setSymbol: 'C',
+                endSymbol: 'M'
+            }
+        ],
+        defaults: {
+            value: 'C',
+            lessThan: 4
+        }
+    },
+    {
+        rules: [
+            {
+                value: 4,
+                startSymbol: 'I',
+                setSymbol: 'V',
+                endSymbol: ''
+            },
+            {
+                value: 5,
+                startSymbol: '',
+                setSymbol: 'M',
+                endSymbol: ''
+            },
+            {
+                value: 9,
+                startSymbol: '',
+                setSymbol: 'I',
+                endSymbol: 'X'
+            }
+        ],
+        defaults: {
+            value: 'M',
+            lessThan: 4
+        }
+    }
+];
